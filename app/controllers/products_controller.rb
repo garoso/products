2798d768f7e31a5138767e578eb5c1f6affc2894
garoso <david.fd.251@gmail.com>
2018-10-19 16:33:04 -0500

    Apply delete functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,12 @@ class ProductsController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+
+    redirect_to products_path
+  end
   private
   def product_params
     params.require(:product).permit(:name, :price, category_ids: [])
